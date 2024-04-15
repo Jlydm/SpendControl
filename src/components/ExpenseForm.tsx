4db7eb4010910b5248401db516f312a7d2d062fe
Jlydm <juliancito8790@gmail.com>
2024-04-15 16:48:42 -0500

    Mostrando una alerta si la validación falla en el formulario, definiendo la acción para los nuevos gastos y agregadon UUID al proyecto

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,9 +1,11 @@
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, FormEvent, useState } from "react"
 import type { DraftExpense, Value } from "../types"
 import { categories } from "../data/categories"
 import DatePicker from 'react-date-picker'
 import "react-calendar/dist/Calendar.css"
 import "react-date-picker/dist/DatePicker.css"
+import ErrorMessage from "./ErrorMessage"
+import { useBudget } from "../hooks/useBudget"
 
 export const ExpenseForm = () => {
   const [expense, setExpense] = useState<DraftExpense>({
@@ -12,6 +14,8 @@ export const ExpenseForm = () => {
     category: '',
     date: new Date()
   })
+  const [error, setError] = useState('')
+  const { dispatch } = useBudget()
 
   const handleChage = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
     const { name, value } = e.target
@@ -30,11 +34,25 @@ export const ExpenseForm = () => {
     })
   }
 
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    // validar
+    if(Object.values(expense).includes('')){
+      setError('Todos los campos son obligatorios')
+      return
+    }
+    // Agregar un nuevo gasto
+    dispatch({type: 'add-expense', payload: { expense }})
+  } 
+
   return (
-    <form className="space-y-5">
+    <form className="space-y-5" onSubmit={handleSubmit}>
       <legend
         className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2"
       >Nuevo Gasto</legend>
+
+      {error && <ErrorMessage>{error}</ErrorMessage>}
 
       <div className="flex flex-col gap-2">
         <label
@@ -46,8 +64,8 @@ export const ExpenseForm = () => {
           id="expenseName"
           placeholder="Añade el nombre del gasto"
           className="bg-slate-100 p-2"
-          name="expenseName" 
-          value={expense.expenseName}
+          name="expenseName"
+          onChange={handleChage}
         />
       </div>
 
