dd8153c0dae3d8392b31ade2bae6398d3c25b26d
Jlydm <juliancito8790@gmail.com>
2024-04-16 17:17:21 -0500

    Creando el componente para mostrar los gastos y mostrando los gastos junto con su icono correspondiente por categoria

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import BudgetForm from "./components/BudgetForm"
 import { useBudget } from "./hooks/useBudget"
 import BudgetTracker from "./components/BudgetTracker"
 import ExpenseModal from "./components/ExpenseModal"
+import ExpenseList from "./components/ExpenseList"
 
 function App() {
 
@@ -17,12 +18,14 @@ function App() {
           Planificador de Gastos
         </h1>
       </header>
+
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
         {isValidBudget ? <BudgetTracker/> : <BudgetForm />}
       </div>
+
       {isValidBudget && (
         <main className="max-w-3xl mx-auto py-10">
-
+          <ExpenseList />
           <ExpenseModal />
         </main>
       )}
